e0d96ead02880591940505991d6fc929bb3f814c
Ruslan <mingalimov2011@yandex.ru>
2023-07-19 00:44:53 +0300

    02-VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,55 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp,defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+const App = defineComponent({
+   name:'App',
+
+   data() {
+      return {       
+         
+         actions:{
+            firstImput : 5, 
+            secondImput: 6,
+            action:'',
+            result:0,
+         }
+      }
+   },
+   methods: {
+      
+   },
+
+   computed:{
+      calculationActions(){
+         
+         switch(this.actions.action) {
+            case 'sum':  
+           
+                this.actions.result = this.actions.firstImput + this.actions.secondImput;
+                break
+          
+            case 'subtract':  
+                this.actions.result = this.actions.firstImput - this.actions.secondImput;
+                break
+
+            case 'multiply':  
+                this.actions.result = this.actions.firstImput * this.actions.secondImput;
+                break
+
+            case 'divide':  
+                this.actions.result = this.actions.firstImput / this.actions.secondImput;     
+                break          
+          }
+
+          
+         return this.actions.result = this.actions.result;    
+      }
+   } 
+
+   });
+
+   const app =createApp(App);
+   const vm = app.mount('#app');
+
+   window.vm = vm;
